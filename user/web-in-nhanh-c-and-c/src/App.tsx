6e3dd030aebfa3acf37fa8b3ecba25fce347a63b
ledangxuanbach <20130199@st.hcmuaf.edu.vn>
2024-08-04 09:30:04 +0700

    update signup, signin, forgotpass page and add background, logo

--- user/web-in-nhanh-c-and-c/src/App.tsx
+++ user/web-in-nhanh-c-and-c/src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import {ThemeProvider} from './context/ThemeContext';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from './context/ThemeContext';
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import FeatureSectionFruits from "./components/FeatureSectionFruits";
@@ -25,47 +25,52 @@ const App = () => {
     return (
         <ThemeProvider>
             <Router>
-                <ScrollToTop/>
-                <Navbar/>
-                <Breadcrumb/>
                 <Routes>
-                    <Route path="/" element={
-                        <main>
-                            <div className="relative z-10" style={{width: '100%', height: '60vh', marginTop: '4vh'}}>
-                                <Hero/>
-                            </div>
-                            <FeatureSectionFruits/>
-                            <FeatureSectionBreakfast/>
-                            <BannerSection/>
-                            <Newsletter/>
-                            <FeatureSectionServiceShop/>
-                            <FeatureSectionInfoShop/>
-                        </main>
-                    }/>
-                    <Route path="/product/:id" element={
-                        <ProductDetail
-                            id={0}
-                            images={["/product__1.webp", "/product__2.webp", "/product__3.webp"]}
-                            name="Xoài Sấy"
-                            price="$20"
-                            description="Xoài sấy ngon với vị ngọt và chua."
-                            rating={4}
-                            reviews={[
-                                {rating: 5, comment: "Tuyệt vời!", image: "/review_image_1.jpg"},
-                                {rating: 4, comment: "Rất tốt, nhưng hơi ngọt.", video: "/review_video_1.mp4"}
-                            ]}
-                        />
-                    }/>
-                    <Route path="/signin" element={<SignIn/>}/>
-                    <Route path="/signup" element={<SignUp/>}/>
-                    <Route path="/forgotpass" element={<ForgotPass/>}/>
-                    <Route path="/cart" element={<Cart/>}/>
-                    <Route path="/checkout" element={<Checkout/>}/>
-                    <Route path="/products" element={<ProductList/>}/>
-                    <Route path="/order-history" element={<OrderHistory />} />
-
+                    <Route path='/*' element={
+                        <>
+                            <ScrollToTop />
+                            <Navbar />
+                            <Breadcrumb />
+                            <Routes>
+                                <Route path="/home" element={
+                                    <main>
+                                        <div className="relative z-10" style={{ width: '100%', height: '60vh', marginTop: '4vh' }}>
+                                            <Hero />
+                                        </div>
+                                        <FeatureSectionFruits />
+                                        <FeatureSectionBreakfast />
+                                        <BannerSection />
+                                        <Newsletter />
+                                        <FeatureSectionServiceShop />
+                                        <FeatureSectionInfoShop />
+                                    </main>
+                                } />
+                                <Route path="/product/:id" element={
+                                    <ProductDetail
+                                        id={0}
+                                        images={["/product__1.webp", "/product__2.webp", "/product__3.webp"]}
+                                        name="Xoài Sấy"
+                                        price="$20"
+                                        description="Xoài sấy ngon với vị ngọt và chua."
+                                        rating={4}
+                                        reviews={[
+                                            { rating: 5, comment: "Tuyệt vời!", image: "/review_image_1.jpg" },
+                                            { rating: 4, comment: "Rất tốt, nhưng hơi ngọt.", video: "/review_video_1.mp4" }
+                                        ]}
+                                    />
+                                } />
+                                <Route path="/cart" element={<Cart />} />
+                                <Route path="/checkout" element={<Checkout />} />
+                                <Route path="/products" element={<ProductList />} />
+                                <Route path="/order-history" element={<OrderHistory />} />
+                            </Routes>
+                            <Footer />
+                        </>
+                    } />
+                    <Route path="/signin" element={<SignIn />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="/forgotpass" element={<ForgotPass />} />
                 </Routes>
-                <Footer/>
             </Router>
         </ThemeProvider>
     );
